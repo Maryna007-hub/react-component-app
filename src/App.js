5d7b7938655ddfa18c450cd3c653fb822e292d8a
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-07 11:38:27 +0100

    sum

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+      
+      <h1>Hello</h1>
+      <footer>
+          This project was coded by 
+           <a href='/'> Maryna Holinka </a>
+           and is <a href='https://github.com/Maryna007-hub/digital-ocean-tutorial'
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          rel="noopener noreferrer">
+     open-soursed on GitHub
         </a>
-      </header>
+        </footer>
     </div>
   );
 }

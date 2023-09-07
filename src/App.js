e294312c45ae2b011da639aa6834c36b2beb8141
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-07 17:10:44 +0100

    upd

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import TopButtons from './components/TopButtons';
 
 function App() {
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-32 px-32 bg-gradient-to-br 
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br 
     from-cyan-600 to-blue-700 shadow-xl shadow-gray-400 rounded-[7px]">
       <TopButtons/>
     </div>

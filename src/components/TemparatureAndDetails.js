4e83bdfeb2f991939f32209fbff2ad8a54a9a82c
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-07 23:18:10 +0100

    upd

--- src/components/TemparatureAndDetails.js
+++ src/components/TemparatureAndDetails.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 function TemparatureAndDetails() {
   return (
-    <div>TemparatureAndDetails</div>
+    <div>
+        <div className='flex items-center justify-center py-6 text-xl text-cyan-300'>
+            <p> Cloudy or whatever</p>
+        </div>
+        <div className='flex  flex-row items-center justify-between py-3 text-white'>
+            <img src='http://openweathermap.org/img/wn/01d@2x.png' alt='' className='w-20'/>
+            <p className='flex flex-col space-y-2'>30°</p>
+        </div>
+    </div>
   )
 }
 

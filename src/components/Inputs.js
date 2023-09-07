b621c2d48266430336719ee5ad9f76869440d962
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-07 18:08:16 +0100

    upd

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -8,10 +8,21 @@ function Inputs() {
         <input type='text'
         placeholder='Search for a city...'
         className='text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase'/>
-      <UilSearch/>
+      <UilSearch size={27} 
+      className='text-white cursor-pointer transition ease-out hover:scale-125'/>
+      
+      <UilLocationPoint size={27} 
+      className='text-white cursor-pointer transition ease-out hover:scale-125'/>
+      
+      </div>
+      <div className='flex flex-row w-1/4 items-center justify-center'>
+         <button name='metric' className='text-xl text-white font-light mx-1'>
+            °C</button>  
+         <button name='imperial' className='text-xl text-white font-light mx-1'>
+           °F</button>
       </div>
     </div>
   );
 }
 
-export default Inputs
+export default Inputs;

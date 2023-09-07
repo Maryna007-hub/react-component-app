4792e1824415d06dc557ea0db9ed307dbc566d96
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-07 17:48:49 +0100

    upd

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -3,7 +3,14 @@ import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons';
 
 function Inputs() {
   return (
-    <div>Inputs</div>
+    <div className='flex flex-row justify-center my-6'>
+      <div className='flex flex-row w-3/4 items-center space-x-4'>
+        <input type='text'
+        placeholder='Search for a city...'
+        className='text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase'/>
+      <UilSearch/>
+      </div>
+    </div>
   );
 }
 

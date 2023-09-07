163162266c56aaf682e854437ab0fa40db15e8f8
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-07 14:42:43 +0100

    upd

--- src/components/TopButtons.js
+++ src/components/TopButtons.js
@@ -1,6 +1,29 @@
 import React from 'react'
 
 function TopButtons() {
+  const cities = [
+    {
+      id:1,
+      title: 'Lisbon'
+    },
+    {
+      id:2,
+      title: 'Madrid'
+    },
+    {
+      id:3,
+      title: 'Paris'
+    },
+    {
+      id:4,
+      title: 'Berlin'
+    },
+    {
+      id:5,
+      title: 'London'
+    }
+    
+  ]
   return (
     <div>TopButtons</div>
   )
